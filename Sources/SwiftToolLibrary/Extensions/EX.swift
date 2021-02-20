972563cb50cb5163d4a3eee7bd3af10168b2b870
zcc <coderzcca@gmail.com>
2021-02-20 14:36:02 +0800

    no message

--- Sources/SwiftToolLibrary/Extensions/EX.swift
+++ Sources/SwiftToolLibrary/Extensions/EX.swift
@@ -18,3 +18,24 @@ extension EXCompatible {
         get { EX(value: self) }
     }
 }
+//extension EXCompatible {
+//    public static var ex: EX<Self>.Type {
+//        set {}
+//        get { EX.self }
+//    }
+//}
+//
+//class Person: EXCompatible {
+//
+//}
+//
+//extension EX where T == Person {
+//    static func test() {
+//
+//    }
+//}
+//
+//func test() {
+//
+//    Person.ex.test()
+//}
