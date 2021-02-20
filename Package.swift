2bfd1603af058f252ec22f7bff13238fb27105d7
zcc <coderzcca@gmail.com>
2021-02-20 13:54:29 +0800

    no message

--- Package.swift
+++ Package.swift
@@ -9,7 +9,7 @@ let package = Package(
     products: [
         .library(
             name: "SwiftToolLibrary",
-            targets: ["Extensions"]),
+            targets: ["SwiftToolLibrary", "Extensions"]),
     ],
     targets: [
         .target(
